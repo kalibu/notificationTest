cdf23372673adcbfac8ee7d5a3d514fe021c17f0
David Pitanguy <davidpitanguy@gmail.com.br>
2024-05-15 15:02:11 -0300

    Updating readme and adding config to run on localhost.

--- src/main/java/test/david/notificationTest/controller/api/LogHistoryApi.java
+++ src/main/java/test/david/notificationTest/controller/api/LogHistoryApi.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import test.david.notificationTest.dto.NotificationDTO;
@@ -17,6 +18,7 @@ import java.util.List;
 @Validated
 @Tag(name = "logHistory", description = "Controller for history of logs")
 @RestController
+@CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
 public interface LogHistoryApi {
 
     /**
--- src/main/java/test/david/notificationTest/controller/api/NotificationApi.java
+++ src/main/java/test/david/notificationTest/controller/api/NotificationApi.java
@@ -9,16 +9,14 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import test.david.notificationTest.dto.NotificationDTO;
 import test.david.notificationTest.dto.SendNotificationDTO;
 
 @Validated
 @Tag(name = "notification", description = "Controller for notification")
 @RestController
+@CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
 public interface NotificationApi {
 
     /**
